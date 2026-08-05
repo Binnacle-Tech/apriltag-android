0f6164380401407544c6bbdb687b7d5ede759c99
Binnacle-Tech <grapplegames+Binnacle@gmail.com>
2026-08-05 16:31:19 -0700

    androidx: migrate off com.android.support; PreferenceFragmentCompat settings

--- src/androidTest/java/edu/umich/eecs/april/apriltag/ApplicationTest.java
+++ src/androidTest/java/edu/umich/eecs/april/apriltag/ApplicationTest.java
@@ -1,13 +1,7 @@
 package edu.umich.eecs.april.apriltag;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
-
-/**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+/*
+ * The legacy android.test.ApplicationTestCase-based test has been removed;
+ * that framework no longer exists in modern Android. Add instrumented tests
+ * here using androidx.test if desired.
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
-    }
-}
